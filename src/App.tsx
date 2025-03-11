1ee6c20ce92644171b310ad6abc4446e18f0e88d
SSM <ssm102030@naver.com>
2025-03-11 13:37:57 +0900

    fix : 배포 오류 해결결

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/member/member.login';
 import JoinPage from './pages/member/member.join';
