2fd60b8367b5bf20513b37d61504792e91016c6f
hyunsunny3 <hs9473@naver.com>
2025-03-15 17:37:06 +0900

    fix : 레이아웃 메인 반영

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/member/member.login';
 import JoinPage from './pages/member/member.join';
+import MainPage from './pages/index';
 import { Provider } from 'react-redux';
 import store from './store';
 import { Global } from "@emotion/react";
@@ -12,6 +13,7 @@ const App = () => {
       <Global styles={globalStyles} />
       <Router>
         <Routes>
+          <Route path="/" element={<MainPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/join" element={<JoinPage />} />
         </Routes>
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/common/Layout';
+
+function MainPage() {
+  return (
+    <Layout>
+      
+    </Layout>
+  );
+}
+
+export default MainPage;
