94eb5393ca40aa30f5b6fe86b209c7222fdf6171
SSM <ssm102030@naver.com>
2025-03-17 19:13:46 +0900

    fix : 병합 오류 해결

---
+++ src/components/common/layout.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+interface LayoutProps {
+  children?: ReactNode; // children을 선택적(?)으로 변경
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <header>
+      <Header />
+      <main>{children || ''}</main>
+      <Footer />
+    </header>
+  );
+};
+
+export default Layout;
