08d518f36e06ddc2eb2b08a8139fd4e3089d0cbd
SSM <ssm102030@naver.com>
2025-03-19 01:24:37 +0900

    feat: 상품 상세 페이지 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/member/LoginPage';
 import JoinPage from './pages/member/JoinPage';
 import MainPage from './pages/IndexPage';
+import ProductDetailPage from './pages/ProductDetailPage';
 import { Provider } from 'react-redux';
 import store from './store';
 import { Global } from '@emotion/react';
@@ -15,6 +16,7 @@ const App = () => {
       <Router>
         <Routes>
           <Route path={PATHS.main} element={<MainPage />} />
+          <Route path={PATHS.detail} element={<ProductDetailPage />} />
           <Route path={PATHS.login} element={<LoginPage />} />
           <Route path={PATHS.join} element={<JoinPage />} />
         </Routes>
