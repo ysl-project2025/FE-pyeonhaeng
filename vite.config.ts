4dcb6f614fbb39f4dfe96c14f7b8a9cc04a4ba9a
hyunsunny3 <hs9473@naver.com>
2025-03-18 17:37:32 +0900

    fix: 스타일 적용 에러 해결 및 app.tsx 라우트 변경

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    jsxImportSource: '@emotion/react', // 👈 Emotion을 JSX 기본 변환기로 설정
+      babel: {
+        plugins: ['@emotion/babel-plugin'], // 👈 Emotion 관련 Babel 플러그인 추가
+      },
+  })],
   base: './',
 });
