49c54634e478816b04218c6f1502ab6c677d3222
SSM <ssm102030@naver.com>
2025-03-19 01:24:16 +0900

    refector : 로그인 회원가입 훅 개선

--- src/hooks/product/useProductList.ts
+++ src/hooks/product/useProductList.ts
@@ -1,10 +1,8 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { AllProduct, ProductApiResponse } from '../../types/product';
-
-const url = import.meta.env.VITE_BASE_URL;
-
-const useProductList = () => {
+const url = import.meta.env.VITE_API_BASE_URL;
+const useProductList = (page: number) => {
   const [products, setProducts] = useState<AllProduct[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
@@ -13,8 +11,14 @@ const useProductList = () => {
     const fetchProducts = async () => {
       try {
         setLoading(true);
-        const response = await axios.get<ProductApiResponse>(`${url}/product`);
-        setProducts(response.data.data || []);
+        const response = await axios.get<ProductApiResponse>(
+          `${url}/product?page=${page}`,
+        );
+        console.log(response.data.data);
+        setProducts((prevProducts) => [
+          ...prevProducts,
+          ...(response.data.data || []),
+        ]);
       } catch (err) {
         console.error(err);
         setError('상품 목록을 불러오는 중 오류가 발생했습니다.');
@@ -24,7 +28,7 @@ const useProductList = () => {
     };
 
     fetchProducts();
-  }, []);
+  }, [page]); // 페이지가 변경될 때마다 호출
 
   return { products, loading, error };
 };
